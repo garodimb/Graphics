ee3e1b804e4e357bf96f7f4447e341986b3945aa
Mahendra Garodi <garodimb@gmail.com>
2016-01-13 00:52:42 +0530

    Integrated keyboard handler

--- input_demo.cpp
+++ input_demo.cpp
@@ -3,6 +3,10 @@
 
 #include <iostream>
 
+#define KEY_ESC 27
+#define KEY_ENTER 13
+
+#define MAX_VERTICES 100
 using namespace std;
 
 int initWindow(char *title,int w,int h);
@@ -11,10 +15,23 @@ void reshape(int w,int h);
 void display(void);
 void mouseHandler(int button,int state,int x,int y);
 void keyboardHandelr(unsigned char key,int x,int y);
+int addVertex(int x,int y);
+int drawPolygon();
+
+struct Point{
+	int x, y;	
+	};
+
+
+Point points[MAX_VERTICES];
+int vCount=-1;
+int screenWidth,screenHeight;
 
 int initWindow(const char *title,int w,int h){
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(w,h);
+	screenWidth=w;
+	screenHeight=h;
 	glutInitWindowPosition(20,20);
 	glutCreateWindow(title);
 	return 0;
@@ -22,12 +39,14 @@ int initWindow(const char *title,int w,int h){
 	
 void init(void){
 	glClearColor(0.0,0.0,0.0,0.0);
+	glPointSize(2.0);
 	glShadeModel(GL_FLAT);	
 	}
 
 void mouseHandler(int button,int state,int x,int y){
 	if(button==GLUT_LEFT_BUTTON && state == GLUT_DOWN){
 		cout<<"Left button Pressed at ("<<x<<","<<y<<")"<<endl;
+		addVertex(x,screenHeight-y);
 		}
 	else if(button==GLUT_LEFT_BUTTON && state == GLUT_UP){
 		cout<<"Left button Released at ("<<x<<","<<y<<")"<<endl;
@@ -41,26 +60,55 @@ void mouseHandler(int button,int state,int x,int y){
 	}
 
 void keyboardHandelr(unsigned char key,int x,int y){
-	cout<<"Key Pressed: "<<key<<endl;
+	cout<<"Key Pressed: "<<(int)key<<endl;
+	if(key==27){
+		cout<<"ESC key pressed. Exiting Program"<<endl;
+		exit(0);
+		}
+	else if(key==KEY_ENTER){
+		drawPolygon();
+		}
 	}	
 
-		
+
 void reshape(int w,int h){
-	glViewport(0,0,(GLsizei) w, (GLsizei) h);
+	cout<<"In Reshape"<<endl;
+	glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+	gluOrtho2D(0.0,(GLdouble) w,0.0, (GLdouble) h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0.0,(GLdouble) w,0.0, (GLdouble) h);
+	screenWidth=w;
+	screenHeight=h;	
 	}
 	
 void display(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	cout<<"In Display"<<endl;
+	glColor3f(1.0,1.0,1.0);
+	drawPolygon();
+	addVertex(100,100);
 	glFlush();
 	}
 	
+int addVertex(int x,int y){
+	vCount++;
+	points[vCount].x=x;
+	points[vCount].y=y;
+	glBegin(GL_POINTS);
+		glVertex2d(screenWidth-x,screenHeight-y);
+	glEnd();
+	cout<<"Adding vertx("<<x<<","<<y<<")"<<endl;
+	glFlush();
+	return 0;
+	}
+
+
+int drawPolygon(){
+	return 0;
+	}
 int main(int argc,char **argv){
 	glutInit(&argc,argv);
-	initWindow("First Demo",1000,500);
+	initWindow("Input Demo",1000,500);
 	init();
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
