9a4cb93513d234c20f651d15438ab8b80ccc5762
Mahendra Garodi <garodimb@gmail.com>
2016-01-12 13:14:52 +0530

    Callback function registration(Keyboard,Mouse)

---
+++ input_demo.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <GL/glut.h>
+
+#include <iostream>
+
+using namespace std;
+
+int initWindow(char *title,int w,int h);
+void init(void);
+void reshape(int w,int h);
+void display(void);
+void mouseHandler(int button,int state,int x,int y);
+void keyboardHandelr(unsigned char key,int x,int y);
+
+int initWindow(const char *title,int w,int h){
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(w,h);
+	glutInitWindowPosition(20,20);
+	glutCreateWindow(title);
+	return 0;
+	}
+	
+void init(void){
+	glClearColor(0.0,0.0,0.0,0.0);
+	glShadeModel(GL_FLAT);	
+	}
+
+void mouseHandler(int button,int state,int x,int y){
+	if(button==GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+		cout<<"Left button Pressed at ("<<x<<","<<y<<")"<<endl;
+		}
+	else if(button==GLUT_LEFT_BUTTON && state == GLUT_UP){
+		cout<<"Left button Released at ("<<x<<","<<y<<")"<<endl;
+		}
+	else if(button==GLUT_RIGHT_BUTTON && state == GLUT_DOWN){
+		cout<<"Right button Pressed at ("<<x<<","<<y<<")"<<endl;
+		}
+	else if(button==GLUT_RIGHT_BUTTON && state == GLUT_UP){
+		cout<<"Right button Released at ("<<x<<","<<y<<")"<<endl;
+		}
+	}
+
+void keyboardHandelr(unsigned char key,int x,int y){
+	cout<<"Key Pressed: "<<key<<endl;
+	}	
+
+		
+void reshape(int w,int h){
+	glViewport(0,0,(GLsizei) w, (GLsizei) h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0,(GLdouble) w,0.0, (GLdouble) h);
+	}
+	
+void display(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	cout<<"In Display"<<endl;
+	glFlush();
+	}
+	
+int main(int argc,char **argv){
+	glutInit(&argc,argv);
+	initWindow("First Demo",1000,500);
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouseHandler);
+	glutKeyboardFunc(keyboardHandelr);
+	glutMainLoop();
+	return 0;
+	}
+
